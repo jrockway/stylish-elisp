50e562f3a247b3bf0b504afd37829d69194e2c58
Jonathan Rockway <jon@jrock.us>
2011-02-21 23:02:05 -0600

    add keywords

--- stylish.el
+++ stylish.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2010  Jonathan Rockway
 
 ;; Author: Jonathan Rockway <jon@jrock.us>
-;; Keywords:
+;; Keywords: programming, repl
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
